9f5f29e99f2aa721e9722a1e2da3c4a6adfd10e9
Maria Fedoseeva <m.s.fedoseeva99@gmail.com>
2021-01-13 16:18:17 +0300

    filters freights

--- django_trucking/freights/urls.py
+++ django_trucking/freights/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
 path("", views.FreightsView.as_view()),
+path("filter/", views.FilterFreightsView.as_view(), name='filter'),
 path("<slug:slug>/", views.FreightDetailView.as_view(), name="freight_detail"),
 path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
 path("worker/<str:slug>/", views.WorkerView.as_view(), name="worker_detail"),
--- django_trucking/freights/views.py
+++ django_trucking/freights/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-from .models import Freight, Category, Worker
+from .models import Freight, Category, Worker, Type
 from .forms import ReviewForm
-class FreightsView(ListView):
+from django.db.models import Q
+
+
+class TypeYear:
+    """Типы и года выхода фильмов"""
+    def get_types(self):
+        return Type.objects.all()
+
+    def get_years(self):
+        return Freight.objects.filter(draft=False).values("year")
+
+class FreightsView(TypeYear, ListView):
     """Список грузов"""
     model = Freight
     queryset = Freight.objects.filter(draft=False)
@@ -11,7 +22,7 @@ class FreightsView(ListView):
    
     
 
-class FreightDetailView(DetailView):
+class FreightDetailView(TypeYear, DetailView):
     """Полное описание грузоперевозки"""
     model = Freight
     slug_field = "url"
@@ -29,8 +40,17 @@ class AddReview(View):
                 form.save()
                 return redirect(freight.get_absolute_url())
 
-class WorkerView(DetailView):
+class WorkerView(TypeYear, DetailView):
     """Вывод информации о работнике"""
     model = Worker
     template_name = 'freights/worker.html'
     slug_field = "name"
+
+class FilterFreightsView(TypeYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Freight.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(types__in=self.request.GET.getlist("type"))
+        )
+        return queryset
